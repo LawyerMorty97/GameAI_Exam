15928e66476411cefbe36d2f0d61f1bf5ad25223
Mathias Berntsen <mathiastb13@gmail.com>
2019-04-29 14:19:01 +0200

    Restarting

--- Project/Assets/Scripts/GameManager.cs
+++ Project/Assets/Scripts/GameManager.cs
@@ -4,18 +4,12 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public enum Team
+    public enum GameState
     {
-        Red,
-        Blue
-    };
-
-    public struct Teammate
-    {
-        public GameObject player;
-        public Team team;
-    };
-
+        Paused,
+        Keeper,
+        Player
+    }
     public static GameManager instance = null;
 
     public GameObject Ball
@@ -23,11 +17,6 @@ public class GameManager : MonoBehaviour
         get;
         private set;
     }
-
-    public GameObject RedTeam;
-    public GameObject BlueTeam;
-
-    private List<Teammate> players;
 
     void Awake()
     {
@@ -39,76 +28,15 @@ public class GameManager : MonoBehaviour
         Ball = GameObject.FindGameObjectWithTag("Ball");
     }
 
-    // Get a player's teammates
-    private List<Teammate> GetTeamMembers(Teammate player)
-    {
-        List<Teammate> mates = new List<Teammate>();
-
-        foreach (Teammate mate in players)
-        {
-            if (mate.team != player.team)
-                mates.Add(mate);
-        }
-
-        mates.Remove(player);
-
-        return mates;
-    }
-
     // Start is called before the first frame update
     void Start()
     {
-        players = new List<Teammate>();
-
-        for (int i = 0; i < RedTeam.transform.childCount; i++)
-        {
-            GameObject player = RedTeam.transform.GetChild(i).gameObject;
-
-            Teammate mate = new Teammate
-            {
-                player = player,
-                team = Team.Red
-            };
-
-            players.Add(mate);
-        }
-
-        for (int i = 0; i < BlueTeam.transform.childCount; i++)
-        {
-            GameObject player = BlueTeam.transform.GetChild(i).gameObject;
-
-            Teammate mate = new Teammate
-            {
-                player = player,
-                team = Team.Blue
-            };
-
-            players.Add(mate);
-        }
-
-        foreach (Teammate mate in players)
-        {
-            Rigidbody rb = mate.player.AddComponent<Rigidbody>();
-            rb.constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
-        }
-
-        Debug.Log("There are " + players.Count.ToString() + " players total.");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        foreach (Teammate mate in players)
-        {
-            mate.player.transform.LookAt(Ball.transform);
-            float mag = Vector3.Distance(mate.player.transform.position, Ball.transform.position);
-            float percent = (mag / 25f) * 1;
-            Color distColor = Color.Lerp(Color.red, Color.black, percent);
-
-            Vector3 normed = (mate.player.transform.position + Ball.transform.position).normalized;
-            mate.player.transform.position = Ball.transform.position + (normed * 5f);
-
-            Debug.DrawLine(mate.player.transform.position, Ball.transform.position, distColor, 0);
-        }
+        
     }
 }
---
+++ Project/Assets/Scripts/StateMachine.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateMachine : MonoBehaviour
+{
+    private enum State
+    {
+        Idle,
+        ChaseBall,
+        TrackBall, // Keeper State
+    }
+
+    private State state = State.Idle;
+
+    public GameObject ball;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ball = GameObject.FindGameObjectWithTag("Ball");
+    }
+
+    public void Transition()
+    {
+        Vector3 heading = ball.transform.position - transform.position;
+        float ballDist = heading.magnitude;
+
+        if (tag != "Keeper")
+        {
+            if (state == State.Idle)
+            {
+                if (ballDist > 2.0f)
+                {
+                    state = State.ChaseBall;
+                }
+            }
+            else if (state == State.ChaseBall)
+            {
+                if (ballDist < 2.0f)
+                {
+                    state = State.Idle;
+                }
+            }
+        } else
+        {
+            if (state == State.Idle)
+            {
+                if (ballDist < 2f)
+                {
+                    state = State.TrackBall;
+                }
+            }
+            else if (state == State.TrackBall)
+            {
+                if (ballDist < 2.0f)
+                {
+                    state = State.Idle;
+                }
+            }
+        }
+    }
+
+    public void Action()
+    {
+        Vector3 directionNormalized = (transform.position - ball.transform.position).normalized;
+
+        switch(state)
+        {
+            case State.ChaseBall:
+                //transform.position -= directionNormalized * (Time.deltaTime * 5f);
+                break;
+            case State.TrackBall:
+                Vector3 keeper_position = new Vector3(transform.position.x, transform.position.y, transform.position.z + directionNormalized.z);
+                transform.position = keeper_position * (Time.deltaTime * 5f);
+                break;
+        }
+    }
+}
